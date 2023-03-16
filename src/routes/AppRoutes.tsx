f75f30bd5fccbdeba6bc58b4c276da6e506b5d8a
iagovtr <iagovitor_araujo@hotmail.com>
2023-03-15 21:31:15 -0300

    feat: initial commit

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/dashboard" element={<></>} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
